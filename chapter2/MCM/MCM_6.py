1db7dc958094f7eecaaec1571a7b1890f7a8e75a
duyifan <2442339079@qq.com>
2024-09-21 11:12:16 +0800

    提交第一次作业

--- chapter2/MCM/MCM_6.py
+++ chapter2/MCM/MCM_6.py
@@ -16,7 +16,7 @@ Gd_bar_weights = longest_path_matrix * -1
 f_vertor = np.zeros((num_of_delay,num_of_delay+1))
 inf = float('inf')
 f_vertor[:,0] = np.array([0,inf]) # 需修改
-print('initial f_vertor is \n:',f_vertor)
+# print('initial f_vertor is \n:',f_vertor)
 
 for j_f in range(1, num_of_delay+1):
     for i_f in range(num_of_delay):
@@ -29,14 +29,14 @@ for j_f in range(1, num_of_delay+1):
                                         f_vertor[i_f,j_f]
                                         )
 
-print('f_vertor is :\n',f_vertor)
+# print('f_vertor is :\n',f_vertor)
 
 result_1 = np.zeros((num_of_delay,num_of_delay))
 for i in range(num_of_delay):
     for m in range(num_of_delay):
         result_1[i,m] = (f_vertor[i,num_of_delay] - f_vertor[i,m]) / (num_of_delay-m)
 
-print('result_1 is :\n',result_1)
+# print('result_1 is :\n',result_1)
 
 max_result_1 = np.max(result_1, axis=1)
 iteration_bound = np.min(max_result_1)*(-1)
